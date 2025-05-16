590dfa4bc82eb161749f0bf4abc4a6501d344e36
Nguyen Xuan Tien <tiennx@hpt.vn>
2025-05-16 12:01:59 +0700

    refactor: remove ChatSupport component and update homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-'use client';
-import ChatSupport from "@/src/components/ui/chat/chat-support";
+"use client";
 
 export default function Home() {
   return (
     <div className="min-w-[100vw] min-h-[100vh] flex justify-center bg-gray-100">
-      <ChatSupport />
+      Truy cập chat bot tại đường dẫn{" "}
+      <strong>
+        <a href="/chatbot">/chatbot</a>
+      </strong>
+      <br />
     </div>
   );
 }
