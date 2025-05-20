86698cf5332bef493a204c31dbeac5655b678338
Nguyen Xuan Tien <tiennx@hpt.vn>
2025-05-20 15:02:34 +0700

    feat: update LoginForm button styles and remove unused dashboard page

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -70,7 +70,7 @@ export function LoginForm({
             </a>
           </div>
         </div>
-        <Button type="submit" className="w-full cursor-pointer">
+        <Button type="submit" className="w-full cursor-pointer dark:text-secondary-foreground">
           Login
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
